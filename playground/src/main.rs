152f71d75b9f220b1bf78164d44463f1ed62e4bc
ammuiyer <ammu.iyer5@gmail.com>
2023-08-07 15:42:53 -0700

    testing some functions

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,5 +1,15 @@
 
-
+fn add_five(num: u32) -> u32 {
+    num+5
+}
 fn main() {
+    let mut x: u32 = 50;
     println!("Hello, world!");
+
+    let y: u32 = add_five(x);
+
+    x= 80;
+    println!("x is {}", x);
+
+
 }
